d65600cb01822eb020c64db654ee16c4f1800035
Wuvist <wuvist@gmail.com>
2020-04-11 01:59:33 +0800

    refactor decode method

---
+++ dechoconf.go
@@ -0,0 +1,85 @@
+package dechoconf
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+// PrefixTagName defines the tagname used in "-" field for prefix
+var PrefixTagName string = "prefix"
+
+type decodeObjFunc func(val interface{}, obj interface{}) error
+
+func decode(configs map[string]interface{}, decodeObj decodeObjFunc, objs ...interface{}) (err error) {
+	prefixToObj := make(map[string]interface{}, len(objs))
+	objToPrefix := make(map[interface{}]string, len(objs))
+	for _, obj := range objs {
+		tt := reflect.ValueOf(obj).Elem().Type()
+		prefixTag, found := tt.FieldByName("_")
+		if !found {
+			return errors.New("No `-` field is found on struct: " + tt.Name())
+		}
+		prefix := prefixTag.Tag.Get(PrefixTagName)
+
+		if o, found := prefixToObj[prefix]; found {
+			return fmt.Errorf("Duplicated prefix %s on struct %s and %s", prefix,
+				reflect.ValueOf(o).Elem().Type().Name(), tt.Name())
+		}
+		prefixToObj[prefix] = obj
+		objToPrefix[obj] = prefix
+	}
+
+	for configPrefix, configVal := range configs {
+
+	FINDOBJ:
+		for obj, objPrefix := range objToPrefix {
+			if configPrefix == objPrefix {
+				if err != decodeObj(configVal, obj) {
+					return err
+				}
+
+				delete(prefixToObj, objPrefix)
+				continue FINDOBJ
+			} else if strings.HasPrefix(objPrefix, configPrefix+".") {
+				currentPrefix := configPrefix + "."
+				val, ok := configVal.(map[string]interface{})
+
+			MATCHFIELD:
+				for ok {
+					ok = false
+
+					for k, v := range val {
+						if objPrefix == currentPrefix+k {
+							if err != decodeObj(v, obj) {
+								return err
+							}
+
+							delete(prefixToObj, objPrefix)
+							continue FINDOBJ
+						} else {
+							if strings.HasPrefix(objPrefix, currentPrefix+k+".") {
+								val, ok = v.(map[string]interface{})
+								currentPrefix = currentPrefix + k + "."
+								continue MATCHFIELD
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	if len(prefixToObj) > 0 {
+		msg := ""
+		for k, o := range prefixToObj {
+			msg += fmt.Sprintf("No config found for: type %s with prefix [%s]\n",
+				reflect.ValueOf(o).Elem().Type().Name(), k)
+		}
+		return errors.New(msg)
+	}
+
+	return nil
+}
+
--- toml.go
+++ toml.go
@@ -2,17 +2,10 @@ package dechoconf
 
 import (
 	"bytes"
-	"errors"
-	"fmt"
 	"io/ioutil"
-	"reflect"
-	"strings"
 
 	"github.com/BurntSushi/toml"
 )
-
-// PrefixTagName defines the tagname used in "-" field for prefix
-var PrefixTagName string = "prefix"
 
 func encodeToml(obj interface{}) (result string, err error) {
 	var buf bytes.Buffer
@@ -26,7 +19,7 @@ func encodeToml(obj interface{}) (result string, err error) {
 	return buf.String(), nil
 }
 
-func decodeObj(tomlVal interface{}, obj interface{}) error {
+func decodeObjToml(tomlVal interface{}, obj interface{}) error {
 	// todo: re-encode & decode is kind of stupid, but works for now
 	tomlString, err := encodeToml(tomlVal)
 	if err != nil {
@@ -44,74 +37,7 @@ func DecodeToml(data string, objs ...interface{}) (err error) {
 		return err
 	}
 
-	prefixToObj := make(map[string]interface{}, len(objs))
-	objToPrefix := make(map[interface{}]string, len(objs))
-	for _, obj := range objs {
-		tt := reflect.ValueOf(obj).Elem().Type()
-		prefixTag, found := tt.FieldByName("_")
-		if !found {
-			return errors.New("No `-` field is found on struct: " + tt.Name())
-		}
-		prefix := prefixTag.Tag.Get(PrefixTagName)
-
-		if o, found := prefixToObj[prefix]; found {
-			return fmt.Errorf("Duplicated prefix %s on struct %s and %s", prefix,
-				reflect.ValueOf(o).Elem().Type().Name(), tt.Name())
-		}
-		prefixToObj[prefix] = obj
-		objToPrefix[obj] = prefix
-	}
-
-	for configPrefix, configVal := range configs {
-
-	FINDOBJ:
-		for obj, objPrefix := range objToPrefix {
-			if configPrefix == objPrefix {
-				if err != decodeObj(configVal, obj) {
-					return err
-				}
-
-				delete(prefixToObj, objPrefix)
-				continue FINDOBJ
-			} else if strings.HasPrefix(objPrefix, configPrefix+".") {
-				currentPrefix := configPrefix + "."
-				val, ok := configVal.(map[string]interface{})
-
-			MATCHFIELD:
-				for ok {
-					ok = false
-
-					for k, v := range val {
-						if objPrefix == currentPrefix+k {
-							if err != decodeObj(v, obj) {
-								return err
-							}
-
-							delete(prefixToObj, objPrefix)
-							continue FINDOBJ
-						} else {
-							if strings.HasPrefix(objPrefix, currentPrefix+k+".") {
-								val, ok = v.(map[string]interface{})
-								currentPrefix = currentPrefix + k + "."
-								continue MATCHFIELD
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
-	if len(prefixToObj) > 0 {
-		msg := ""
-		for k, o := range prefixToObj {
-			msg += fmt.Sprintf("No config found for: type %s with prefix [%s]\n",
-				reflect.ValueOf(o).Elem().Type().Name(), k)
-		}
-		return errors.New(msg)
-	}
-
-	return nil
+	return decode(configs, decodeObjToml, objs...)
 }
 
 // DecodeTomlFile accept path of a toml file, and unmarshal it to multiple structs
